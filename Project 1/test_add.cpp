4c3958121d21992bf60a993f6daf248a71dc1962
JinkLeft <opeio1@gmail.com>
2020-09-13 11:59:22 -0400

    Added test for + operator
    Added test for + operator -JINK

---
+++ Project 1/test_add.cpp
@@ -0,0 +1,89 @@
+// bigint Test Program
+//
+// Tests:  int constructor, uses ==
+//
+// NEED TO IMPLEMENT
+//
+#include <iostream>
+#include <cassert>
+#include "bigint.hpp"
+
+
+//===========================================================================
+int main () {
+    {
+
+        // VERY INCOMPLETE set of tests.
+
+
+        //------------------------------------------------------
+        // Setup fixture
+        bigint left(0);
+        bigint right(0);
+        bigint result;
+
+        // Test 
+        result = left + right;
+
+        // Verify
+        assert(left   == 0);
+        assert(right  == 0);
+        assert(result == 0);
+    }
+
+    {
+      //------------------------------------------------------
+      // Setup fixture
+      bigint left(1);
+      bigint right(0);
+      bigint result;
+
+      // Test
+      result = left + right;
+
+      // Verify
+      assert(left   == 1);
+      assert(right  == 0);
+      assert(result == 1);
+    }
+    {
+      //------------------------------------------------------
+      // Setup fixture
+      bigint left(1);
+      bigint right(1);
+      bigint result;
+
+      // Test
+      result = left + right;
+
+      // Verify
+      assert(left   == 1);
+      assert(right  == 1);
+      assert(result == 2);
+    }
+    {
+      //------------------------------------------------------
+      // Setup fixture
+      // TESTING CHAR CTOR with CHAR CTOR
+      bigint left("123450");
+      bigint right("123451");
+      bigint result;
+
+      // Test
+      result = left + right;
+
+      // Verify
+      assert(left   == 123450);
+      assert(right  == 123451);
+      assert(result == 246901);
+    }
+
+    //FIXED ASSERTION
+
+    
+    //Add test cases as needed.
+    
+    
+    std::cout << "Done with testing addition." << std::endl;
+}
+
