aca6c73785a75dfdb8d5f86b1df8c1f2bbd8ee2a
JinkLeft <opeio1@gmail.com>
2020-09-14 14:25:09 -0400

    add.cpp
    -Added add.cpp -JINK
    -The file reads data1-1.txt and outputs added bigints skipping the ';' and whitespace;
    -BUG: The final output is 0 and 0 which shouldn't be. -JINK

---
+++ Project 1/add.cpp
@@ -0,0 +1,39 @@
+//==============================================================================
+// Basics for openning a file for milestone 2 of Project 1.
+//
+#include <iostream>
+#include <fstream>
+#include <cstdlib>
+#include "bigint.hpp"
+
+int main() {
+    std::ifstream in("data1-1.txt");    // Define stream for input
+    if(!in) {                           // Make sure it opened correctly.
+        std::cerr << "Could not open data1-1.txt, exiting." << std::endl;
+        exit(1);
+    }
+
+	char firstWord[CAPACITY];
+	char secondWord[CAPACITY];
+
+	while(!in.eof()) {
+		in.getline(firstWord, CAPACITY, ';');
+		in.getline(secondWord, CAPACITY, ';');
+
+		bigint b1(firstWord);
+		bigint b2(secondWord);
+
+		bigint b3 = (b1 + b2);
+
+		std::cout << "\n  " << b1 << std::endl;
+		std::cout << "+ " << b2 << std::endl;
+		std::cout << "= " << b3 << std::endl;
+	}
+
+	if(in.eof()) {
+		std::cout << "\nEOF REACHED " << std::endl;
+	}
+
+    in.close();
+    return 0;
+}
